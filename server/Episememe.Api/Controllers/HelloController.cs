2dc41b36e7b82dbd4a9d7928d4f46e3dab903503
marax27 <kacpert314@gmail.com>
2020-03-01 11:36:45 +0100

    Create base Vue and ASP.NET Core apps

---
+++ server/Episememe.Api/Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Episememe.Api.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class HelloController : ControllerBase
+    {
+        private readonly ILogger<HelloController> _logger;
+
+        public HelloController(ILogger<HelloController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world!";
+        }
+    }
+}
---
+++ server/Episememe.UnitTests/SampleUnitTest.cs
@@ -0,0 +1,14 @@
+using FluentAssertions;
+using Xunit;
+
+namespace Episememe.UnitTests
+{
+    public class SampleUnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
