7127e8e1fcdb70e30a3c7c7231e953d614c7138f
marax27 <kacpert314@gmail.com>
2020-03-08 19:13:37 +0100

    Accept any host address

--- server/Episememe.Api/Program.cs
+++ server/Episememe.Api/Program.cs
@@ -20,6 +20,7 @@ namespace Episememe.Api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://[::]:5000");
                     webBuilder.UseStartup<Startup>();
                 });
     }
