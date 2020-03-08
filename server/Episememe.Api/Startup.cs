0bfab36b421c09e19d07ba854e1a7f4fee3b47cd
marax27 <kacpert314@gmail.com>
2020-03-08 18:10:37 +0100

    Add Auth0 config for Production, clean up Startup

--- server/Episememe.Api/Startup.cs
+++ server/Episememe.Api/Startup.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Episememe.Infrastructure.Auth;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Episememe.Api.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Episememe.Api
 {
@@ -26,19 +19,7 @@ namespace Episememe.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(options =>
-            {
-                var auth0Configuration = new Auth0Configuration();
-                Configuration.Bind("Auth0", auth0Configuration);
-
-                options.Authority = auth0Configuration.Authority;
-                options.Audience = auth0Configuration.Audience;
-            });
-
+            services.AddJwtAuthentication(Configuration);
             services.AddControllers();
         }
 
