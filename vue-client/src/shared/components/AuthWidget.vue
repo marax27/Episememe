7b24175f9210b760122d8d9695904665955083b6
marax27 <kacpert314@gmail.com>
2020-03-14 12:07:25 +0100

    Create NavBar and AuthWidget components

---
+++ vue-client/src/shared/components/AuthWidget.vue
@@ -0,0 +1,32 @@
+<template>
+  <span v-if="$auth.loading">
+    Loading...
+  </span>
+  <div v-else>
+    <div v-if="$auth.isAuthenticated">
+      <button @click="logout">Log out</button>
+    </div>
+    <button v-else @click="login">Log in</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AuthWidget',
+  components: {},
+  methods: {
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-client/src/shared/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav>
+    <div class="left">
+      <router-link to="/">Home</router-link> | 
+      <router-link to="/about">About</router-link>
+    </div>
+    <div class="right" style='height: 100%'>
+      <AuthWidget/>
+    </div>
+    <div style='clear: both'></div>
+  </nav>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue';
+import AuthWidget from './AuthWidget.vue';
+
+export default Vue.extend({
+  name: 'NavBar',
+  components: {
+    AuthWidget
+  }
+});
+</script>
+
+<style scoped>
+nav {
+  padding: 30px;
+  border-bottom: solid 1px #42b983;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
