823fedd36d3d8963e5bb9aff4132199e50412e7a
marax27 <kacpert314@gmail.com>
2020-03-16 08:52:01 +0100

    Clean up

--- vue-client/src/views/Home.vue
+++ vue-client/src/views/Home.vue
@@ -3,3 +3,14 @@
     <img alt="Vue logo" src="../assets/logo.png">
   </div>
 </template>
+
+<script lang='ts'>
+import { Component, Mixins, Vue } from 'vue-property-decorator';
+
+@Component({
+  name: 'Home'
+})
+export default class Home extends Vue {
+}
+</script>
+
