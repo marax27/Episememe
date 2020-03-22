0129b693e493fa214298218a6841133d0c218195
marax27 <kacpert314@gmail.com>
2020-03-22 23:30:39 +0100

    Set primary color

--- vue-client/src/plugins/vuetify.ts
+++ vue-client/src/plugins/vuetify.ts
@@ -1,7 +1,17 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import colors from 'vuetify/lib/util/colors';
+
+const lightColorPalette = {
+    primary: colors.purple.darken4,
+};
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: lightColorPalette
+        }
+    }
 });
