7dada71967b741108256205362d55e5de3e35e5c
marax27 <kacpert314@gmail.com>
2020-04-03 16:30:32 +0200

    Clean up configuration

---
+++ server/Episememe.Api/Configuration/Cors.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Episememe.Api.Configuration
+{
+    public static class Cors
+    {
+        public static IApplicationBuilder EnableCors(this IApplicationBuilder app)
+        {
+            app.UseCors(o => {
+                o.AllowAnyOrigin();
+                o.AllowAnyHeader();
+                o.AllowAnyMethod();
+            });
+            return app;
+        }
+    }
+}
