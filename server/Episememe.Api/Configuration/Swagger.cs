a8ce1beafa166efcee84104ccfa531fe0853b9ac
marax27 <kacpert314@gmail.com>
2020-04-03 16:28:04 +0200

    Install Swagger, configure it as development-only

---
+++ server/Episememe.Api/Configuration/Swagger.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Episememe.Api.Configuration
+{
+    public static class Swagger
+    {
+        public static IServiceCollection ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Episememe API", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Provide bearer token for the Authorization header. Example: \"Bearer {token}\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                        }, new string[]{ }
+                    }
+                });
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder ExposeSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Episememe API v1");
+                c.RoutePrefix = string.Empty;
+            });
+            return app;
+        }
+    }
+}
