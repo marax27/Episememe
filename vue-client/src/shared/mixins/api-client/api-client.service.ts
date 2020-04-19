009beea2a0ca73cb0084b95a5d51831d0a97e715
marax27 <kacpert314@gmail.com>
2020-04-19 18:28:54 +0200

    Store API URL in .env file

--- vue-client/src/shared/mixins/api-client/api-client.service.ts
+++ vue-client/src/shared/mixins/api-client/api-client.service.ts
@@ -1,5 +1,4 @@
 import { Vue, Component } from 'vue-property-decorator';
-import { apiUrl } from '../../../../api.config.json';
 import ApiClient from './api-client';
 
 @Component({
@@ -11,6 +10,6 @@ export default class ApiClientService extends Vue {
   $auth: any;  // it's necessary, as API calls require a token
 
   created() {
-    this.$api = new ApiClient(this.$auth, apiUrl);
+    this.$api = new ApiClient(this.$auth, process.env.VUE_APP_API_URL);
   }
 }
