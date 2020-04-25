08337072dd2555d7062052c833eecd7caaf29b38
SlawekKalandyk <slawomir.kalandyk@gmail.com>
2020-04-25 21:49:34 +0200

    Create MediaController

---
+++ server/Episememe.Api/Controllers/MediaController.cs
@@ -0,0 +1,38 @@
+﻿using Episememe.Application.DataTransfer;
+using Episememe.Application.Features.SearchMedia;
+using Episememe.Application.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Episememe.Api.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    [Authorize]
+    public class MediaController : ControllerBase
+    {
+        private readonly ILogger<MediaController> _logger;
+        private readonly IMediator _mediator;
+
+        public MediaController(ILogger<MediaController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("media")]
+        public async Task<IEnumerable<MediaInstanceDto>> GetSearchedMedia([FromBody] SearchMediaDto searchMediaDto)
+        {
+            var query = SearchMediaQuery.Create(searchMediaDto);
+            var result = await _mediator.Send(query);
+            return result;
+        }
+    }
+}
---
+++ server/Episememe.Application/DataTransfer/MediaInstanceDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Episememe.Application.DataTransfer
+{
+    public class MediaInstanceDto
+    {
+        public string Id { get; }
+        public IEnumerable<string> Tags { get; }
+
+        public MediaInstanceDto(string id, IEnumerable<string> tags)
+        {
+            Id = id;
+            Tags = tags;
+        }
+    }
+}
