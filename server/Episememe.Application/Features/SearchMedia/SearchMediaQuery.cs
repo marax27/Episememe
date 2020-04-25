00930957715d4b1cefc3cdfadab66d5cbf851096
SlawekKalandyk <slawomir.kalandyk@gmail.com>
2020-04-25 21:43:29 +0200

    Create SearchMediaQuery/QueryHandler

---
+++ server/Episememe.Application/Features/SearchMedia/SearchMediaQuery.cs
@@ -0,0 +1,25 @@
+﻿using Episememe.Application.DataTransfer;
+using Episememe.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Episememe.Application.Features.SearchMedia
+{
+    public class SearchMediaQuery : IRequest<IEnumerable<MediaInstanceDto>>
+    {
+        public SearchMediaDto SearchMedia { get; }
+
+        private SearchMediaQuery(SearchMediaDto searchMedia)
+        {
+            SearchMedia = searchMedia;
+        }
+
+        public static SearchMediaQuery Create(SearchMediaDto searchMedia)
+        {
+            return new SearchMediaQuery(searchMedia);
+        }
+    }
+}
---
+++ server/Episememe.Application/Features/SearchMedia/SearchMediaQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using Episememe.Application.DataTransfer;
+using Episememe.Application.Features.MediaFiltering;
+using Episememe.Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Episememe.Application.Features.SearchMedia
+{
+    public class SearchMediaQueryHandler : RequestHandler<SearchMediaQuery, IEnumerable<MediaInstanceDto>>
+    {
+        private IApplicationContext _context;
+
+        public SearchMediaQueryHandler(IApplicationContext context)
+            => _context = context;
+
+        protected override IEnumerable<MediaInstanceDto> Handle(SearchMediaQuery request)
+        {
+            var filteredMedia = new MediaFilter(request.SearchMedia)
+                .Filter(_context.MediaInstances)
+                .Select(mi =>
+                    new MediaInstanceDto(mi.Id, mi.MediaTags.Select(mt => mt.Tag.Name))
+                );
+
+            return filteredMedia;
+        }
+    }
+}
