5015d4688fdf2a35a405c9b66e8ef17a26356ea3
marax27 <kacpert314@gmail.com>
2020-04-26 14:34:13 +0200

    Create MediaComponent

---
+++ vue-client/src/browsing/media-gallery/media-component/MediaComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <img :src='mediaUrl'
+       :alt='`Cannot display #${instance.id}`'/>
+</template>
+
+<script lang='ts'>
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { IMediaInstance } from '../../../shared/models/IMediaInstance';
+
+@Component
+export default class MediaComponent extends Vue {
+  @Prop()
+  instance?: IMediaInstance;
+
+  public get mediaUrl(): string {
+    return 'http://localhost:18888/' + this.instance?.id;
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
