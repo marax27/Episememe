334d9fee03d48739d7af475c5765ecba387e776c
prznoc <przemyslaw.nocon19@gmail.com>
2020-04-27 11:39:56 +0200

    Create LoadController

---
+++ server/Episememe.Api/Controllers/LoadController.cs
@@ -0,0 +1,35 @@
+using Episememe.Application.DataTransfer;
+using System.Threading.Tasks;
+using Episememe.Api.Utilities;
+using Episememe.Application.Features.LoadMedia;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Episememe.Api.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    [Authorize]
+    public class LoadController : ControllerBase
+    {
+        private readonly ILogger<LoadController> _logger;
+        private readonly IMediator _mediator;
+
+        public LoadController(ILogger<LoadController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("load")]
+        public async Task<LoadInstanceDto> GetSearchedMedia([FromBody] LoadMediaDto loadMediaDto)
+        {
+            var query = LoadMediaQuery.Create(loadMediaDto);
+            var result = await _mediator.Send(query);
+            return result;
+        }
+    }
+}
