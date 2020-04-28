9dc29b4738f3bf52bca8d798a06e7357eb19e6f0
SlawekKalandyk <slawomir.kalandyk@gmail.com>
2020-04-28 17:48:55 +0200

    Change SearchMediaDto properties to include setters

--- server/Episememe.Application/DataTransfer/SearchMediaDto.cs
+++ server/Episememe.Application/DataTransfer/SearchMediaDto.cs
@@ -1,23 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Episememe.Application.DataTransfer
 {
     public class SearchMediaDto
     {
-        public IEnumerable<string> IncludedTags { get; } = null!;
-        public IEnumerable<string> ExcludedTags { get; } = null!;
-        public DateTime? TimeRangeStart { get; } = null;
-        public DateTime? TimeRangeEnd { get; } = null;
-
-        public SearchMediaDto(IEnumerable<string> includedTags, IEnumerable<string> excludedTags,
-            DateTime? timeRangeStart, DateTime? timeRangeEnd)
-        {
-            IncludedTags = includedTags;
-            ExcludedTags = excludedTags;
-            TimeRangeStart = timeRangeStart;
-            TimeRangeEnd = timeRangeEnd;
-        }
+        public IEnumerable<string> IncludedTags { get; set; } = null!;
+        public IEnumerable<string> ExcludedTags { get; set; } = null!;
+        public DateTime? TimeRangeStart { get; set; }
+        public DateTime? TimeRangeEnd { get; set; }
     }
 }
