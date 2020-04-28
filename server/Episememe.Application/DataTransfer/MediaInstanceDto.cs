3209cc9663f4a4f44e1ee50c769681b7d0509e11
SlawekKalandyk <slawomir.kalandyk@gmail.com>
2020-04-28 17:44:34 +0200

    Add DataType to MediaInstanceDto

--- server/Episememe.Application/DataTransfer/MediaInstanceDto.cs
+++ server/Episememe.Application/DataTransfer/MediaInstanceDto.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Episememe.Application.DataTransfer
 {
     public class MediaInstanceDto
     {
         public string Id { get; }
+        public string DataType { get; }
         public IEnumerable<string> Tags { get; }
 
-        public MediaInstanceDto(string id, IEnumerable<string> tags)
+        public MediaInstanceDto(string id, string dataType, IEnumerable<string> tags)
         {
             Id = id;
+            DataType = dataType;
             Tags = tags;
         }
     }
