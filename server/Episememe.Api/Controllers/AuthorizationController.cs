10d6eb5e623f5bf1f741455f9a29678587a3b038
marax27 <kacpert314@gmail.com>
2020-05-01 21:20:59 +0200

    Create IssueBrowseTokenCommandHandler

---
+++ server/Episememe.Api/Controllers/AuthorizationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Episememe.Application.Features.IssueBrowseToken;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Episememe.Api.Controllers
+{
+    [ApiController]
+    [Route("api/authorization")]
+    [Authorize]
+    public class AuthorizationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthorizationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<string> Post()
+        {
+            var browseToken = Guid.NewGuid().ToString();
+
+            var command = IssueBrowseTokenCommand.Create(browseToken);
+            await _mediator.Send(command);
+            return browseToken;
+        }
+    }
+}
---
+++ server/Episememe.Application.Tests/FeatureTests/IssueBrowseToken/WhenIssuingBrowseToken.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Episememe.Application.Features.IssueBrowseToken;
+using Episememe.Application.Interfaces;
+using Episememe.Application.Tests.Helpers;
+using Episememe.Domain.Entities;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace Episememe.Application.Tests.FeatureTests.IssueBrowseToken
+{
+    public class WhenIssuingBrowseToken
+    {
+        private readonly DateTime _givenDate = new DateTime(2020, 5, 1);
+        private readonly ISet<BrowseToken> _givenBrowseTokens = new HashSet<BrowseToken>
+        {
+            new BrowseToken {Id = "xyz", ExpirationTime = new DateTime(2020, 3, 1)},
+            new BrowseToken {Id = "qwe", ExpirationTime = new DateTime(2020, 6, 1)}
+        };
+
+        private readonly Mock<ITimeProvider> _timeProviderMock;
+        private readonly Mock<DbSet<BrowseToken>> _browseTokensMock;
+        private readonly Mock<IAuthorizationContext> _authorizationContextMock;
+
+        private readonly ISet<BrowseToken> _addedTokens;
+        private readonly ISet<BrowseToken> _removedTokens;
+
+        public WhenIssuingBrowseToken()
+        {
+            _addedTokens = new HashSet<BrowseToken>();
+            _removedTokens = new HashSet<BrowseToken>();
+
+            _timeProviderMock = new Mock<ITimeProvider>();
+            _timeProviderMock.Setup(provider => provider.GetUtc())
+                .Returns(_givenDate);
+
+            _browseTokensMock = DbSetMockFactory.Create(_givenBrowseTokens);
+            _browseTokensMock.StoreAddedEntitiesIn(_addedTokens);
+            _browseTokensMock.StoreRemovedEntitiesIn(_removedTokens);
+
+            _authorizationContextMock = new Mock<IAuthorizationContext>();
+            _authorizationContextMock.Setup(context => context.BrowseTokens)
+                .Returns(_browseTokensMock.Object);
+        }
+
+        [Fact]
+        public void GivenSampleToken_ThenSingleTokenIsAdded()
+        {
+            var command = IssueBrowseTokenCommand.Create("AB12ab");
+
+            var sut = new IssueBrowseTokenCommandHandler(_authorizationContextMock.Object, _timeProviderMock.Object);
+            sut.Handle(command, CancellationToken.None).Wait();
+
+            _addedTokens.Count.Should().Be(1);
+        }
+
+        [Fact]
+        public void GivenSampleToken_ThenTokenHasExpectedValue()
+        {
+            var command = IssueBrowseTokenCommand.Create("AB12ab");
+
+            var sut = new IssueBrowseTokenCommandHandler(_authorizationContextMock.Object, _timeProviderMock.Object);
+            sut.Handle(command, CancellationToken.None).Wait();
+
+            _addedTokens.Should().Contain(token => token.Id == "AB12ab");
+        }
+
+        [Fact]
+        public void GivenSampleToken_ThenExpirationTimeIsInTheFuture()
+        {
+            var command = IssueBrowseTokenCommand.Create("AB12ab");
+
+            var sut = new IssueBrowseTokenCommandHandler(_authorizationContextMock.Object, _timeProviderMock.Object);
+            sut.Handle(command, CancellationToken.None).Wait();
+
+            _addedTokens.Should().OnlyContain(token => token.ExpirationTime > _givenDate);
+        }
+
+        [Fact]
+        public void GivenSampleToken_ThenExpiredTokensAreRemoved()
+        {
+            var expectedRemovedTokens = _givenBrowseTokens.Where(token => token.ExpirationTime <= _givenDate);
+            var command = IssueBrowseTokenCommand.Create("AB12ab");
+
+            var sut = new IssueBrowseTokenCommandHandler(_authorizationContextMock.Object, _timeProviderMock.Object);
+            sut.Handle(command, CancellationToken.None).Wait();
+
+            _removedTokens.Should().BeEquivalentTo(expectedRemovedTokens);
+        }
+    }
+}
