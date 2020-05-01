8dcdef7c0a7340c6652ec91981f52a80b47169d9
marax27 <kacpert314@gmail.com>
2020-05-01 19:02:29 +0200

    Add BrowseToken to database

--- server/Episememe.Infrastructure/Database/ApplicationDbContext.cs
+++ server/Episememe.Infrastructure/Database/ApplicationDbContext.cs
@@ -16,6 +16,8 @@ namespace Episememe.Infrastructure.Database
         public DbSet<Tag> Tags { get; set; } = null!;
         public DbSet<MediaTag> MediaTags { get; set; } = null!;
 
+        public DbSet<BrowseToken> BrowseTokens { get; set; } = null!;
+
         public void Migrate() => Database.Migrate();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
