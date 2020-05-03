a11a48966395a8a18a9ab9508cd7d21402a71e66
prznoc <przemyslaw.nocon19@gmail.com>
2020-05-03 18:54:24 +0200

    Create TagController

---
+++ server/Episememe.Api/Controllers/TagController.cs
@@ -0,0 +1,36 @@
+using System.Threading.Tasks;
+using Episememe.Api.Utilities;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using Episememe.Application.Features.GetTags;
+using Episememe.Application.DataTransfer;
+
+namespace Episememe.Api.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    [Authorize]
+    public class TagController : ControllerBase
+    {
+        private readonly ILogger<TagController> _logger;
+        private readonly IMediator _mediator;
+
+        public TagController(ILogger<TagController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("tag")]
+        public async Task<IEnumerable<TagInstanceDto>> GetTags()
+        {
+            var query = GetTagsQuery.Create();
+            var result = await _mediator.Send(query);
+            return result;
+        }
+    }
+}
