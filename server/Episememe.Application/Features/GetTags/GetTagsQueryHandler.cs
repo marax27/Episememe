dc105e01cbec4fc9a9d4942ad9dbb558278a519c
prznoc <przemyslaw.nocon19@gmail.com>
2020-05-03 18:54:47 +0200

    Create TagQuery with Handler

---
+++ server/Episememe.Application/Features/GetTags/GetTagsQueryHandler.cs
@@ -0,0 +1,24 @@
+using Episememe.Application.DataTransfer;
+using Episememe.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Episememe.Application.Filtering.BaseFiltering;
+
+namespace Episememe.Application.Features.GetTags
+{
+    public class TagQueryHandler : RequestHandler<GetTagsQuery, IEnumerable<TagInstanceDto>>
+    {
+        private readonly IApplicationContext _context;
+
+        public TagQueryHandler(IApplicationContext context)
+            => _context = context;
+
+        protected override IEnumerable<TagInstanceDto> Handle(GetTagsQuery request)
+        {
+            var Tags = _context.Tags.Select(mi => new TagInstanceDto(mi.Name, mi.Description));
+            return Tags;
+        }
+    }
+}
