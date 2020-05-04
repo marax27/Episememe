e172c67dfb3b3e5171db79b38ed870dae4e3dfc1
marax27 <kacpert314@gmail.com>
2020-05-04 19:27:44 +0200

    Create migration

--- server/Episememe.Infrastructure/Migrations/20200504172719_MakeTagNameUnique.Designer.cs
+++ server/Episememe.Infrastructure/Migrations/20200504172719_MakeTagNameUnique.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Episememe.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Episememe.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200504172719_MakeTagNameUnique")]
+    partial class MakeTagNameUnique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ server/Episememe.Infrastructure/Migrations/20200504172719_MakeTagNameUnique.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Episememe.Infrastructure.Migrations
+{
+    public partial class MakeTagNameUnique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_Name",
+                table: "Tags",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tags_Name",
+                table: "Tags");
+        }
+    }
+}
