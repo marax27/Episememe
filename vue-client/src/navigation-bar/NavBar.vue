8660bd04a7fbd52fac9ce61d69d56b8713894c37
marax27 <kacpert314@gmail.com>
2020-05-06 18:10:44 +0200

    Create NavBarLink

--- vue-client/src/navigation-bar/NavBar.vue
+++ vue-client/src/navigation-bar/NavBar.vue
@@ -1,18 +1,11 @@
 <template>
   <v-app-bar app id="nav-bar">
     <div class="d-flex align-center">
-      <v-img
-        alt="Vuetify Logo"
-        class="shrink mr-2"
-        contain
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-        transition="scale-transition"
-        width="40"/>
 
-      <router-link to="/" class="title nav-link white--text">Episememe</router-link>
+      <NavBarLink link='/' icon='mdi-hexagon-outline' :title='true'>Episememe</NavBarLink>
 
-      <router-link to="/upload" class="nav-link white--text">Upload</router-link>
-      <router-link to="/revision" class="nav-link white--text">Revision</router-link>
+      <NavBarLink link='/upload' icon='mdi-upload'>Upload</NavBarLink>
+      <NavBarLink link='/revision' icon='mdi-pencil-box-outline'>Revision</NavBarLink>
     </div>
 
     <v-spacer></v-spacer>
@@ -24,34 +17,14 @@
 <script lang='ts'>
 import { Vue, Component } from 'vue-property-decorator';
 import AuthWidget from './components/AuthWidget.vue';
+import NavBarLink from './components/NavBarLink.vue';
 
 @Component({
   name: 'NavBar',
   components: {
-    AuthWidget
+    AuthWidget,
+    NavBarLink
   }
 })
 export default class NavBar extends Vue {}
 </script>
-
-<style scoped>
-.nav-link {
-  text-decoration: none;
-  border-bottom: solid 1px;
-  border-bottom-color: transparent;
-}
-.nav-link:hover {
-  border-bottom-color: rgba(256, 256, 256, 0.7);
-}
-.nav-link:active {
-  border-bottom-color: rgba(256, 256, 256, 1);
-}
-
-.nav-link + .nav-link {
-  margin-left: 1em;
-}
-
-a:active, a:focus {
-  outline: 0;
-}
-</style>
---
+++ vue-client/src/navigation-bar/components/NavBarLink.vue
@@ -0,0 +1,65 @@
+<template>
+  <router-link
+    :to='link'
+    class='nav-link white--text'
+    v-bind:class='{ "title": title }'>
+
+    <v-icon v-if='!!icon'
+            :left='!isSmall'
+            :large='!title && isSmall'
+            :x-large='title'>
+      {{ icon }}
+    </v-icon>
+
+    <span v-show='!isSmall'>
+      <slot></slot>
+    </span>
+
+  </router-link>
+</template>
+
+<script lang='ts'>
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class NavBarLink extends Vue {
+  @Prop()
+  link!: string;
+
+  @Prop({ default: '' })
+  icon!: string;
+
+  @Prop({ default: false })
+  title!: boolean;
+
+  get isSmall(): boolean {
+    return this.$vuetify.breakpoint.xsOnly;
+  }
+}
+</script>
+
+<style scoped>
+.nav-link {
+  text-decoration: none;
+  border-bottom: solid 1px;
+  border-bottom-color: transparent;
+  display: flex;
+  align-items: center;
+}
+
+.nav-link:hover {
+  border-bottom-color: rgba(256, 256, 256, 0.7);
+}
+.nav-link:active {
+  border-bottom-color: rgba(256, 256, 256, 1);
+}
+
+.nav-link + .nav-link {
+  margin-left: .5em;
+  margin-right: .5em;
+}
+
+a:active, a:focus {
+  outline: 0;
+}
+</style>
