c1180b3a8e7359d4ab16417ce1bbff3b6be4e3a0
SlawekKalandyk <slawomir.kalandyk@gmail.com>
2020-05-09 01:12:33 +0200

    Add access to transactions to IWritableApplicationContext

--- server/Episememe.Application/Interfaces/IWritableApplicationContext.cs
+++ server/Episememe.Application/Interfaces/IWritableApplicationContext.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Episememe.Application.Interfaces
 {
     public interface IWritableApplicationContext : IApplicationContext, IWritableContext
     {
+        DatabaseFacade Database { get; }
     }
 }
