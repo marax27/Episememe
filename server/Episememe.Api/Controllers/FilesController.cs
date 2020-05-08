c061aea52b62aa235060e43892ace0cdd45d0580
SlawekKalandyk <slawomir.kalandyk@gmail.com>
2020-05-09 01:15:16 +0200

    Create file upload endpoint

--- server/Episememe.Api/Controllers/FilesController.cs
+++ server/Episememe.Api/Controllers/FilesController.cs
@@ -1,9 +1,14 @@
-using System.Threading.Tasks;
+using System;
+using Episememe.Application.DataTransfer;
 using Episememe.Application.Features.FileMedia;
+using Episememe.Application.Features.UploadFile;
 using Episememe.Application.Features.VerifyBrowseToken;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Episememe.Api.Utilities;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Episememe.Api.Controllers
 {
@@ -21,6 +26,7 @@ namespace Episememe.Api.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         [Route("files/{id}")]
         public async Task<IActionResult> GetFile(string id, [FromQuery] string token)
         {
@@ -32,6 +38,18 @@ namespace Episememe.Api.Controllers
             var fileQuery = FileMediaQuery.Create(id);
             var result = await _mediator.Send(fileQuery);
             return result;
+        }
+
+        [HttpPost]
+        [Route("files")]
+        public async Task TaskUploadNewFile([FromForm] FileUploadDto fileUploadDto)
+        {
+            if(fileUploadDto?.FormFile == null || fileUploadDto?.Tags == null)
+                throw new ArgumentNullException();
+
+            var user = User.GetUserId();
+            var fileUploadCommand = UploadFileCommand.Create(fileUploadDto.FormFile, fileUploadDto.Tags, user);
+            await _mediator.Send(fileUploadCommand);
         }
     }
 }
