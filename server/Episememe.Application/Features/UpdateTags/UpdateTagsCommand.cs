8b08e1b8ad8ecd1a494ec1919e64cc4762a5fc89
prznoc <przemyslaw.nocon19@gmail.com>
2020-05-13 18:27:51 +0200

    Command and CommandHandler

---
+++ server/Episememe.Application/Features/UpdateTags/UpdateTagsCommand.cs
@@ -0,0 +1,23 @@
+using MediatR;
+using System.Collections.Generic;
+
+namespace Episememe.Application.Features.UpdateTags
+{
+    public class UpdateTagsCommand : IRequest
+    {
+        public string Id { get; }
+        public IEnumerable<string> Tags { get; }
+    
+
+        private UpdateTagsCommand(string id, IEnumerable<string> tags)
+        {
+            Id = id;
+            Tags = tags;
+        }
+
+        public static UpdateTagsCommand Create(string id, IEnumerable<string> tags)
+        {
+            return new UpdateTagsCommand(id, tags);
+        }
+    }
+}
---
+++ server/Episememe.Application/Features/UpdateTags/UpdateTagsCommandHandler.cs
@@ -0,0 +1,55 @@
+using MediatR;
+using Episememe.Application.Interfaces;
+using System.Linq;
+using System.Collections.Generic;
+using Episememe.Domain.Entities;
+using Episememe.Domain.HelperEntities;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+
+namespace Episememe.Application.Features.UpdateTags
+{
+    public class UpdateTagsCommandHandler : RequestHandler<UpdateTagsCommand>
+    {
+        private readonly IWritableApplicationContext _context;
+
+        public UpdateTagsCommandHandler(IWritableApplicationContext context)
+            => _context = context;
+
+        protected override void Handle(UpdateTagsCommand request)
+        {
+            MediaInstance editedInstance;
+            try 
+            {
+                editedInstance = _context.MediaInstances.First(a => a.Id == request.Id);
+            }
+            catch (Exception){
+                throw;
+            }
+
+            ICollection<MediaTag> mediaTags = ConvertStringsToTags(request.Tags, editedInstance)
+            .Select(t => new MediaTag()
+            {
+                MediaInstance = editedInstance,
+                Tag = t
+            }).ToList();
+            editedInstance.MediaTags = mediaTags;
+            _context.SaveChanges();
+        }
+
+        private IEnumerable<Tag> ConvertStringsToTags(IEnumerable<string> stringTags, MediaInstance media)
+        {
+            var tagNames = _context.Tags.Select(t => t.Name);
+            var tags = stringTags.Select(st =>
+                tagNames.Contains(st) ? _context.Tags.Single(t => t.Name == st) : new Tag() { Name = st, Description = st });
+
+            return tags;
+        }
+
+    }
+}
