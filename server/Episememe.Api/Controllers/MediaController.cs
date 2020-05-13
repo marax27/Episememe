4ce81cc7faddc4ddb585afd807b2998818af1af1
prznoc <przemyslaw.nocon19@gmail.com>
2020-05-13 18:16:52 +0200

    Added revision to controller

--- server/Episememe.Api/Controllers/MediaController.cs
+++ server/Episememe.Api/Controllers/MediaController.cs
@@ -1,5 +1,6 @@
 ﻿using Episememe.Application.DataTransfer;
 using Episememe.Application.Features.SearchMedia;
+using Episememe.Application.Features.UpdateTags;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -7,6 +8,7 @@ using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using System.Threading.Tasks;
+using System;
 
 namespace Episememe.Api.Controllers
 {
@@ -32,6 +34,15 @@ namespace Episememe.Api.Controllers
             var query = SearchMediaQuery.Create(searchMediaDto);
             var result = await _mediator.Send(query);
             return result;
+        }
+
+        [HttpPost]
+        [Route("revision/{id}")]
+        public async Task UpdateTagsList(string id, [FromForm] string tags)
+        {
+            IEnumerable<string> _tags = tags.Split(",");
+            var command = UpdateTagsCommand.Create(id, _tags);
+            await _mediator.Send(command);
         }
     }
 }
