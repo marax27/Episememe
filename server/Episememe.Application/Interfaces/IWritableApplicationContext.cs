58ff0b7832858c76eff45bf46dca1ad80eb9111a
prznoc <przemyslaw.nocon19@gmail.com>
2020-05-13 18:28:46 +0200

    Misc

--- server/Episememe.Application/Interfaces/IWritableApplicationContext.cs
+++ server/Episememe.Application/Interfaces/IWritableApplicationContext.cs
@@ -1,6 +1,21 @@
+<<<<<<< HEAD
 ﻿namespace Episememe.Application.Interfaces
 {
     public interface IWritableApplicationContext : IApplicationContext, IWritableContext
     {
     }
 }
+=======
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Episememe.Application.Interfaces
+{
+    public interface IWritableApplicationContext : IApplicationContext, IWritableContext
+    {
+        DatabaseFacade Database { get; }
+    }
+}
+>>>>>>> Misc
