51f9bb6fd0625ec0ef94b6022476615dd88a915c
prznoc <przemyslaw.nocon19@gmail.com>
2020-05-13 18:29:11 +0200

    Filtering of existing tags

--- server/Episememe.Application/Features/UpdateTags/UpdateTagsCommandHandler.cs
+++ server/Episememe.Application/Features/UpdateTags/UpdateTagsCommandHandler.cs
@@ -45,6 +45,8 @@ namespace Episememe.Application.Features.UpdateTags
         private IEnumerable<Tag> ConvertStringsToTags(IEnumerable<string> stringTags, MediaInstance media)
         {
             var tagNames = _context.Tags.Select(t => t.Name);
+            var mediaTagNames = media.MediaTags.Select(t => t.Tag.Name);
+            stringTags = stringTags.Where(t => !(mediaTagNames.Contains(t)));
             var tags = stringTags.Select(st =>
                 tagNames.Contains(st) ? _context.Tags.Single(t => t.Name == st) : new Tag() { Name = st, Description = st });
 
