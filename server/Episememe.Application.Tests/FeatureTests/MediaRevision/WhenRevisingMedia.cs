72e68d7692935360eb6ee3b01a5e620f1d6b465e
prznoc <przemyslaw.nocon19@gmail.com>
2020-05-16 11:27:43 +0200

    Added tests

---
+++ server/Episememe.Application.Tests/FeatureTests/MediaRevision/WhenRevisingMedia.cs
@@ -0,0 +1,98 @@
+using Episememe.Application.Features.UpdateTags;
+using Episememe.Application.Interfaces;
+using Episememe.Domain.Entities;
+using Episememe.Domain.HelperEntities;
+using Episememe.Infrastructure.Database;
+using FluentAssertions;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace Episememe.Application.Tests.FeatureTests.FileRevision
+{
+    public class WhenUploadingFile : IDisposable
+    {
+        private readonly IWritableApplicationContext _contextMock;
+        private readonly DbConnection _connection;
+
+        public WhenUploadingFile()
+        {
+            (_contextMock, _connection) = GetInMemoryDatabaseContext();
+        }
+
+        [Fact]
+        public void SwapTagsInInstance()
+        {
+            var Tags = new List<string> 
+            {"sword", "shield", "minimini"};
+
+            var command = UpdateTagsCommand.Create("k8wetest", Tags);
+            var handler = new UpdateTagsCommandHandler(_contextMock);
+            handler.Handle(command, CancellationToken.None).Wait();
+
+            _contextMock.MediaInstances.Single().MediaTags.Select(t => t.Tag.Name).Should().BeEquivalentTo(Tags);
+        }
+
+        [Fact]
+        public void TryEditNonexistentFile()
+        {
+            var Tags = new List<string> 
+            {"sword", "shield", "minimini"};
+
+            var command = UpdateTagsCommand.Create("xdxdxdxd", Tags);
+            var handler = new UpdateTagsCommandHandler(_contextMock);
+            
+            Exception ex = Assert.Throws<AggregateException>(() => handler.Handle(command, CancellationToken.None).Wait());
+
+            Assert.Equal("File not found", ex.Message);
+        }
+
+        private (IWritableApplicationContext, DbConnection) GetInMemoryDatabaseContext()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlite(connection)
+                .Options;
+            var context = new ApplicationDbContext(options);
+            context.Database.EnsureCreated();
+
+            context.MediaInstances.Add(CreateExampleDatabaseInstance());
+            context.SaveChanges();
+
+            return (context, connection);
+        }
+
+        public MediaInstance CreateExampleDatabaseInstance()
+        {
+            var mediaInstance = new MediaInstance()
+            {
+                Id = "k8wetest",
+                DataType = "file",
+            };
+            var tags = new List<string>()
+            {
+                "pigeons", "flying rats", "little mermaid"
+            };
+            ICollection<MediaTag> mediaTags = tags.Select(t => new MediaTag() 
+            {
+                    MediaInstance = mediaInstance,
+                    Tag = new Tag() {Name = t}
+            }).ToList();
+
+            return mediaInstance;
+        }
+
+          public void Dispose()
+        {
+            _connection?.Dispose();
+        }
+    }
+
+}
