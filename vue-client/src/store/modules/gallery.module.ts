62ba4b2fba37d9067cbc3295dc1c28f11c0f8ed7
marax27 <kacpert314@gmail.com>
2020-05-20 21:11:20 +0200

    Add volume setting to store

--- vue-client/src/store/modules/gallery.module.ts
+++ vue-client/src/store/modules/gallery.module.ts
@@ -4,16 +4,23 @@ import { IMediaInstance } from '@/shared/models/IMediaInstance';
 export const gallery: Module<any, any> = {
   namespaced: true,
   state: () => ({
-    currentMediaInstance: null
+    currentMediaInstance: null,
+    volumeOn: true,
   }),
   mutations: {
     setCurrentMediaInstance(state, newInstance: IMediaInstance) {
       state.currentMediaInstance = newInstance;
+    },
+    setVolumeOn(state, volumeOn: boolean) {
+      state.volumeOn = volumeOn;
     }
   },
   actions: {
     updateCurrentMediaInstance({ commit }, instance: IMediaInstance) {
       commit('setCurrentMediaInstance', instance);
+    },
+    toggleVolume({ commit, state }) {
+      commit('setVolumeOn', !state.volumeOn);
     }
   },
   getters: {
