fa2f39a26848dc226cf426c31770c72381f55765
SlawekKalandyk <slawomir.kalandyk@gmail.com>
2020-05-24 20:13:41 +0200

    Create a command to mark media as favorite

---
+++ server/Episememe.Application/Features/MarkFavoriteMedia/MarkFavoriteMediaCommand.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System;
+
+namespace Episememe.Application.Features.MarkFavoriteMedia
+{
+    public class MarkFavoriteMediaCommand : IRequest
+    {
+        public string MediaInstanceId { get; }
+        public string UserId { get; }
+
+        private MarkFavoriteMediaCommand(string mediaInstanceId, string userId)
+        {
+            MediaInstanceId = mediaInstanceId;
+            UserId = userId;
+        }
+
+        public static MarkFavoriteMediaCommand Create(string mediaInstanceId, string userId)
+        {
+            if (mediaInstanceId == null)
+                throw new ArgumentNullException(nameof(mediaInstanceId));
+            if (string.IsNullOrEmpty(userId))
+                throw new ArgumentNullException(nameof(userId));
+
+            return new MarkFavoriteMediaCommand(mediaInstanceId, userId);
+        }
+    }
+}
---
+++ server/Episememe.Application/Features/MarkFavoriteMedia/MarkFavoriteMediaCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using Episememe.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+using Episememe.Domain.Entities;
+
+namespace Episememe.Application.Features.MarkFavoriteMedia
+{
+    public class MarkFavoriteMediaCommandHandler : IRequestHandler<MarkFavoriteMediaCommand>
+    {
+        private readonly IWritableApplicationContext _context;
+
+        public MarkFavoriteMediaCommandHandler(IWritableApplicationContext context)
+        {
+            _context = context;
+        }
+        public async Task<Unit> Handle(MarkFavoriteMediaCommand request, CancellationToken cancellationToken)
+        {
+            var mediaInstance = await _context.MediaInstances
+                .Include(mi => mi.FavoriteMedia)
+                .SingleAsync(mi => mi.Id == request.MediaInstanceId, cancellationToken);
+
+            var newFavoriteMedia = new FavoriteMedia()
+            {
+                MediaInstance = mediaInstance,
+                UserId = request.UserId
+            };
+
+            mediaInstance.FavoriteMedia.Add(newFavoriteMedia);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
