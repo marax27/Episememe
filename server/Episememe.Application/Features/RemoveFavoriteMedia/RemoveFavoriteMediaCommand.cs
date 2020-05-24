c88e10606dfeb4eae6f9b5d9634f3e1bd0afb479
SlawekKalandyk <slawomir.kalandyk@gmail.com>
2020-05-24 20:14:08 +0200

    Create command to remove media from user's favorites

---
+++ server/Episememe.Application/Features/RemoveFavoriteMedia/RemoveFavoriteMediaCommand.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System;
+
+namespace Episememe.Application.Features.RemoveFavoriteMedia
+{
+    public class RemoveFavoriteMediaCommand : IRequest
+    {
+        public string MediaInstanceId { get; set; }
+        public string UserId { get; set; }
+
+        private RemoveFavoriteMediaCommand(string mediaInstanceId, string userId)
+        {
+            MediaInstanceId = mediaInstanceId;
+            UserId = userId;
+        }
+
+        public static RemoveFavoriteMediaCommand Create(string mediaInstanceId, string userId)
+        {
+            if (mediaInstanceId == null)
+                throw new ArgumentNullException(nameof(mediaInstanceId));
+            if (string.IsNullOrEmpty(userId))
+                throw new ArgumentNullException(nameof(userId));
+
+            return new RemoveFavoriteMediaCommand(mediaInstanceId, userId);
+        }
+    }
+}
---
+++ server/Episememe.Application/Features/RemoveFavoriteMedia/RemoveFavoriteMediaCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Episememe.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Episememe.Application.Features.RemoveFavoriteMedia
+{
+    public class RemoveFavoriteMediaCommandHandler : IRequestHandler<RemoveFavoriteMediaCommand>
+    {
+        private readonly IWritableApplicationContext _context;
+
+        public RemoveFavoriteMediaCommandHandler(IWritableApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(RemoveFavoriteMediaCommand request, CancellationToken cancellationToken)
+        {
+            var mediaInstance = await _context.MediaInstances
+                .Include(mi => mi.FavoriteMedia)
+                .SingleAsync(mi => mi.Id == request.MediaInstanceId, cancellationToken);
+
+            var favoriteMedia = mediaInstance.FavoriteMedia
+                .Single(fm => fm.UserId == request.UserId && fm.MediaInstanceId == mediaInstance.Id);
+
+            mediaInstance.FavoriteMedia.Remove(favoriteMedia);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
