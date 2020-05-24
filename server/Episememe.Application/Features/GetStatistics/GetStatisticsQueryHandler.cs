3c54bb7f7b44b92f24b5a310820b662b738e2a2d
prznoc <przemyslaw.nocon19@gmail.com>
2020-05-24 17:15:20 +0200

    Statistics Query and QueryHandler

---
+++ server/Episememe.Application/Features/GetStatistics/GetStatisticsQueryHandler.cs
@@ -0,0 +1,42 @@
+using Episememe.Application.DataTransfer;
+using Episememe.Application.Interfaces;
+using MediatR;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace Episememe.Application.Features.GetStatistics
+{
+    public class GetStatisticsQueryHandler : RequestHandler<GetStatisticsQuery, MediaTimeDto>
+    {
+        private readonly IApplicationContext _context;
+        private readonly ITimeProvider _timeProvider;
+        public GetStatisticsQueryHandler(IApplicationContext context, ITimeProvider timeProvider)
+        {
+            _context = context;
+            _timeProvider = timeProvider;
+        }
+        protected override MediaTimeDto Handle(GetStatisticsQuery request)
+        {
+            var dates = _context.MediaInstances.Select(x => x.Timestamp).ToList();
+            var start = dates.Min();
+            var end = _timeProvider.GetUtc();
+            var nbOfDailyInstances = dates.GroupBy(x => x.Date)
+                .Select(x => new 
+                {
+                    Value = x.Count(),
+                    Day = (DateTime) x.Key.Date!
+                }).ToList();
+            List<List<long>> statistics = new List<List<long>>();
+            long count = 0;
+            for(DateTime date = start; date.Date <= end.Date; date = date.AddDays(1))
+            {
+                var instance = nbOfDailyInstances.FirstOrDefault(x => x.Day == date.Date);
+                if (instance != null) count += instance.Value;
+                long converted_date = (long) (date.Date - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
+                statistics.Add(new List<long> {converted_date, count});
+            }
+            return new MediaTimeDto {Dates = statistics};
+        }
+    }
+}
