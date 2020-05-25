f0c6d5bd1b83411c1e054de2d83bd79eb9abaddc
SlawekKalandyk <slawomir.kalandyk@gmail.com>
2020-05-25 19:58:50 +0200

    Add filtering by favorite media tests

---
+++ server/Episememe.Application.Tests/Filtering/WhenFilteringMediaByFavorite.cs
@@ -0,0 +1,51 @@
+﻿using Episememe.Application.Features.SearchMedia;
+using Episememe.Application.Filtering.BaseFiltering;
+using Episememe.Application.Tests.Helpers.Contexts.Filtering;
+using Episememe.Domain.Entities;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Episememe.Application.Tests.Filtering
+{
+    public class WhenFilteringMediaByFavorite
+    {
+        [Fact]
+        public void GivenNullFilterArguments_AllMediaAreReturned()
+        {
+            var givenMediaInstances = new FavoriteMediaTestsDbSet().Instances;
+
+            var filteredMedia = GetFilteredMedia(new SearchMediaData(), givenMediaInstances);
+
+            filteredMedia.Should().HaveCount(givenMediaInstances.Count());
+        }
+
+        [Fact]
+        public void GivenUserAndMediaDataSet_UsersFavoriteMediaAreReturned()
+        {
+            var givenUserId = "user1";
+            var givenMediaInstances = new FavoriteMediaTestsDbSet().Instances;
+            var givenSearchMediaData = new SearchMediaData()
+            {
+                UserId = givenUserId
+            };
+
+            var filteredMedia = GetFilteredMedia(givenSearchMediaData, givenMediaInstances);
+
+            filteredMedia.Should().HaveCount(2);
+            filteredMedia.Should().Contain(mi => mi.Id == "1");
+            filteredMedia.Should().Contain(mi => mi.Id == "2");
+        }
+
+        private ISet<MediaInstance> GetFilteredMedia(SearchMediaData searchMedia, DbSet<MediaInstance> mediaInstances)
+        {
+            var favoriteMediaFilter = new FavoriteMediaFilter(searchMedia.UserId);
+            var filteredMedia = favoriteMediaFilter.Filter(mediaInstances.ToList().AsReadOnly())
+                .ToHashSet();
+
+            return filteredMedia;
+        }
+    }
+}
---
+++ server/Episememe.Application.Tests/Helpers/Contexts/Filtering/FavoriteMediaTestsDbSet.cs
@@ -0,0 +1,61 @@
+﻿using Episememe.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Episememe.Application.Tests.Helpers.Contexts.Filtering
+{
+    public class FavoriteMediaTestsDbSet : IDbSetTestContext<MediaInstance>
+    {
+        public DbSet<MediaInstance> Instances => CreateMediaInstancesDbSet();
+
+        private DbSet<MediaInstance> CreateMediaInstancesDbSet()
+        {
+            var user1 = "user1";
+            var user2 = "user2";
+
+            var mediaInstance1 = new MediaInstance
+            {
+                Id = "1"
+            };
+            var mediaInstance2 = new MediaInstance
+            {
+                Id = "2"
+            };
+            var mediaInstance3 = new MediaInstance
+            {
+                Id = "3"
+            };
+
+            var favoriteMedia1 = CreateFavoriteMedia(user1, mediaInstance1);
+            var favoriteMedia2 = CreateFavoriteMedia(user1, mediaInstance2);
+            var favoriteMedia3 = CreateFavoriteMedia(user2, mediaInstance2);
+            var favoriteMedia4 = CreateFavoriteMedia(user2, mediaInstance3);
+
+            mediaInstance1.FavoriteMedia.Add(favoriteMedia1);
+            mediaInstance2.FavoriteMedia.Add(favoriteMedia2);
+            mediaInstance2.FavoriteMedia.Add(favoriteMedia3);
+            mediaInstance3.FavoriteMedia.Add(favoriteMedia4);
+
+            var instances = new HashSet<MediaInstance>()
+            {
+                mediaInstance1,
+                mediaInstance2,
+                mediaInstance3
+            };
+
+            return DbSetMockFactory.Create(instances).Object;
+        }
+
+        private FavoriteMedia CreateFavoriteMedia(string userId, MediaInstance mediaInstance)
+        {
+            var newFavoriteMedia = new FavoriteMedia
+            {
+                UserId = userId,
+                MediaInstance = mediaInstance,
+                MediaInstanceId = mediaInstance.Id
+            };
+
+            return newFavoriteMedia;
+        }
+    }
+}
