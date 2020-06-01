1dc32064c1ae629e9bb557c7d79a43be6e75fdce
marax27 <kacpert314@gmail.com>
2020-06-01 13:12:26 +0200

    Update API and store on click

--- vue-client/src/browsing/settings-menu/buttons/FavoriteMenuButton.vue
+++ vue-client/src/browsing/settings-menu/buttons/FavoriteMenuButton.vue
@@ -7,26 +7,46 @@
 </template>
 
 <script lang='ts'>
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Mixins } from 'vue-property-decorator';
 import BaseSettingsMenuButton from './BaseSettingsMenuButton.vue';
+import ApiClientService from '../../../shared/mixins/api-client/api-client.service';
 
 @Component({
   components: {
     BaseSettingsMenuButton
   }
 })
-export default class FavoriteMenuButton extends Vue {
+export default class FavoriteMenuButton extends Mixins(ApiClientService) {
 
   get isFavorite(): boolean {
     return this.$store.state.favorite.favoriteIds.includes(this.currentMediaId);
   }
 
   toggleFavorite() {
-    this.$store.dispatch('favorite/toggleFavoriteId', this.currentMediaId);
+    const newState = !this.isFavorite;
+    this.toggleFavoriteInStore();
+    this.toggleFavoriteInApi(newState, this.currentMediaId)
+      .catch(_err => {
+        this.toggleFavoriteInStore();
+        this.$store.dispatch('reportError', 'Failed to update your preferences.');
+      });
   }
 
   private get currentMediaId(): string {
-    return this.$store.getters.currentMediaId;
+    return this.$store.getters['gallery/currentMediaId'];
+  }
+
+  private toggleFavoriteInStore() {
+    return this.$store.dispatch('favorite/toggleFavoriteId', this.currentMediaId);
+  }
+
+  private toggleFavoriteInApi(newValue: boolean, mediaId: string) {
+    const url = 'favorite/' + mediaId;
+    if (newValue) {
+      return this.$api.put<void>(url, {});
+    } else {
+      return this.$api.delete<void>(url, {});
+    }
   }
 }
 </script>
