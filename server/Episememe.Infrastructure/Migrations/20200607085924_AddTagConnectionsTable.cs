6516c445f7fd9368c599cc7766e3029e0e2cf592
marax27 <kacpert314@gmail.com>
2020-06-07 11:03:22 +0200

    Add migration

---
+++ server/Episememe.Infrastructure/Migrations/20200607085924_AddTagConnectionsTable.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Episememe.Infrastructure.Migrations
+{
+    public partial class AddTagConnectionsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TagConnections",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EntryEdgeId = table.Column<int>(nullable: false),
+                    DirectEdgeId = table.Column<int>(nullable: false),
+                    ExitEdgeId = table.Column<int>(nullable: false),
+                    Successor = table.Column<int>(nullable: false),
+                    Ancestor = table.Column<int>(nullable: false),
+                    Hops = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TagConnections", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TagConnections");
+        }
+    }
+}
