2e96a608af36da255c81288c34a85d36c21f26f1
Kacper Tonia <kacpert314@gmail.com>
2020-07-19 11:21:08 +0200

    Increase file upload size limit

---
+++ server/Episememe.Api/Configuration/FileUploadLimits.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Episememe.Api.Configuration
+{
+    public static class FileUploadLimits
+    {
+        public static IServiceCollection ConfigureFileUploadLimits(this IServiceCollection services)
+        {
+            services.Configure<KestrelServerOptions>(options =>
+            {
+                options.Limits.MaxRequestBodySize = int.MaxValue;
+            });
+            services.Configure<FormOptions>(options =>
+            {
+                options.ValueLengthLimit = int.MaxValue;
+                options.MultipartBodyLengthLimit = int.MaxValue;
+                options.MultipartHeadersLengthLimit = int.MaxValue;
+            });
+
+            return services;
+        }
+    }
+}
